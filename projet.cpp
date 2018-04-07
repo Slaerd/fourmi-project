b70de3751fbf020f9524ff67f83df20ce4735187
Slaerd <38158233+Slaerd@users.noreply.github.com>
2018-04-07 17:59:10 +0200

    Add files via upload

--- projet.cpp
+++ projet.cpp
@@ -1,12 +1,7 @@
 #include <projet.hpp>
 
-//hello world
 {//Fourmi base
-/*Crée une fourmi ne portant pas de sucre
- * @param a l'indice de la fourmi dans tabFourmi
- * @param c les coordonnées de la fourmi
- * @return f une fourmi
-*/
+
 fourmi creerFourmi(int a, coord c){
     fourmi f;
     f.n = a;
@@ -14,64 +9,37 @@ fourmi creerFourmi(int a, coord c){
     f.c = c;
 }
 
-
-/*Retourne les coordonnées d'une fourmi
- * @param f fourmi
- * @return les coordonnées de la fourmi f
-*/
 coord coordFourmis(fourmi f){
     return f.c;
 }
 
-/*Retourne l'indice d'une fourmi
- * @param f fourmi
- * @return son indice dans le tableau des fourmis
-*/
 int numFourmis(fourmi f){
     return f.n;
 }
 
-/*Retourne vrai si la fourmi porte du sucre
- * @param f la fourmi
- * @return bool
-*/
 bool porteSucre(fourmi f){
     if(f.sucre > 0) return true;
     else return false;
 }
 
-
-/*Retourne vrai si la fourmi rentre au nid
- * @param f la fourmir
- * @return bool
-*/
 bool rentreNid(fourmi f){
     return porteSucre(f);
 }
 
-/*Supprime la charge portée par la fourmi
- * @param f la fourmi
-*/
 void dechargerSucre(fourmi &f){
     f.sucre = 0;
 }
 
-/*Ajoute une charge de sucre à la fourmi
- * @param f la fourmi
-*/
 void chargerSucre(fourmi &f){
     f.sucre++;
 }
 
-
-/*Déplace une fourmi
- * @param f la fourmi
-*/
 void deplaceFourmi(fourmi &f,coord c){
     f.c = c;
 }
 
 }
+
 {//Place base
 
 struct place{
@@ -133,10 +101,15 @@ void poserPheroSucre(place &p, float i){
 	p.phero = i;
 }
 void diminuerPheroSucre(place &p){
-	
-
-void deplacerFourmi(fourmi &f, place &p1, place &p2);
+	if(p.phero != 0) p.phero-= 5;
 }
+
+void deplacerFourmi(fourmi &f, place &p1, place &p2){
+	poserFourmi(f,p2);
+	enleverFourmi(p1);
+}
+}
+
 //Composes
 bool estVide(place p){
 	b = numeroFourmi(p) == -1;
--- projet.hpp
+++ projet.hpp
@@ -30,42 +30,146 @@ struct ensCoord {
 };
 	
 
-//	BASIC fourmi
+{//	BASIC fourmi
+/*Cree une fourmi ne portant pas de sucre
+ * @param a l'indice de la fourmi dans tabFourmi
+ * @param c les coordonnées de la fourmi
+ * @return f une fourmi
+*/
 fourmi creerFourmi(int a, coord c);
 
+/*Retourne les coordonnées d'une fourmi
+ * @param f fourmi
+ * @return les coordonnées de la fourmi f
+*/
 coord coordFourmis(fourmi f);
+
+/*Retourne l'indice d'une fourmi
+ * @param f fourmi
+ * @return son indice dans le tableau des fourmis
+*/
 int numFourmis(fourmi f);
 
+/*Retourne vrai si la fourmi porte du sucre
+ * @param f la fourmi
+ * @return bool
+*/
 bool porteSucre(fourmi f);
+
+/*Retourne vrai si la fourmi rentre au nid
+ * @param f la fourmir
+ * @return bool
+*/
 bool rentreNid(fourmi f);
 
+/*Supprime la charge portée par la fourmi
+ * @param f la fourmi
+*/
 void dechargerSucre(fourmi &f);
-void chargerSucre(fourmi &f);
-void deplaceFourmi(&fourmi, coord);
 
+/*Ajoute une charge de sucre a la fourmi
+ * @param f la fourmi
+*/
+void chargerSucre(fourmi &f);
+
+/*Deplace une fourmi
+ * @param f la fourmi
+*/
+void deplaceFourmi(&fourmi, coord);
+}
 void chargerTabFourmis(fourmi &f; ensCord);
 
+
 //	BASIC place
+/*Cree une place vide
+ *@param c les coordonnées de la place
+ *@return la place
+ */
 place creerPlaceVide(coord c);
 
+/*Donne la coordonnee d'une place
+ *@param p la place
+ *@return la coordonnee
+ */
 coord coordPlace(place p);
+/*Donne l'intensite des pheromones de nid sur la place
+ *@param p la place
+ *@return l'intensite
+ */
 float pheroNid(place p);
+
+/*Donne l'intensite des pheromones de sucre sur la place
+ *@param p la place
+ *@return l'intensite
+ */
 int pheroSucre(place p);
+ 
+/*Donne le numero eventuel de la fourmi sur la place
+ *@param p la place
+ *@return le numero de la fourmi
+ */
 int numeroFourmi(place p);
 
+/*Verifie si une place contient du sucre
+ *@param p la place
+ *@return oui ou non
+ */
 bool contientSucre(place p);
+
+/*Verifie si une place contient un element de nid
+ *@param p la place
+ *@return oui ou non
+ */
 bool contientNid(place p);
+
+/*Verifie si une place contient des pheromones de sucre
+ *@param p la place
+ *@return oui ou non
+ */
 bool surUnePiste(place p);
 
+/*Pose du sucre sur une place
+ *@param/res p la place
+ */
 void poserSucre(place &p);
+
+/*Pose un element de nid sur une place
+ *@param/res p la place
+ */
 void poserNid(place &p);
+
+/*Pose une fourmi sur une place
+ *@param/res f la fourmi
+ *@param/res p la place
+ */
 void poserFourmi(fourmi &f, place &p);
+
+/*Enleve eventuellement une fourmi d'une place
+ *@param/res p la place
+ */
 void enleverFourmi(place &p);
+
+/*Pose une pheromone de nid d'une certaine intensite sur une place
+ *@param/res p la place
+ */
 void poserPheroNid(place &p, float i);
+
+/*Pose des pheromone de sucre d'une certaine intensite sur une place
+ *@param/res p la place
+ */
 void poserPheroSucre(place &p, float i);
+
+/*Diminue l'intensite de la pheromone de sucre de 5
+ *@param/res p la place
+ */
 void diminuerPheroSucre(place &p);
 
-void deplacerFourmi(fourmi &f, place &p1, place &p2);
+/*Deplace une fourmi d'une place a une autre
+ *@param/res f la fourmi
+ *@param/res p1 la place de depart
+ *@param/res p2 la place d'arrivee
+ */
+void deplaceFourmi(fourmi &f, place &p1, place &p2);
 
 //	BASIC grille
 void chargerGrilleVide(grille &g);
