0fbace6dbcd8333dddd75e9f72ed847f924611a2
Vincent-Sivadon <38160250+Vincent-Sivadon@users.noreply.github.com>
2018-04-23 16:42:39 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,6 +15,85 @@ void afficheFourmi(fourmi f){
 
 void testFourmi();
 
+void testCreerFourmi(){
+	coord c; fourmi f;
+	c.x = 3; c.y = 2;
+	f = creerFourmi(4,c);
+	ASSERT(f.n == 4 and f.c = c);
+	c.x = 26; c.y = 3;
+	f = creerFourmi(15,c);
+	ASSERT(f.n == 15 and f.c = c);
+}
+
+
+void testCoordFourmis(){
+	coord c;
+	c.x = 14; c.y = 12;
+	fourmi f = creerFourmi(1,c);
+	ASSERT(coordFourmis(f) == c);
+	c.x = 2; c.y = 2;
+	f = creerFourmi(24,c);
+	ASSERT(coordFourmis(f) == c);
+}
+
+
+void testNumFourmis(){
+	fourmi f;
+	f.n = 1;
+	ASSERT(numFourmis(f) == 1);
+	f.n = 24;
+	ASSERT(numFourmis(f) == 24);
+}
+
+
+void testPorteSucre(){
+	fourmi f;
+	f.sucre = 3;
+	ASSERT(porteSucre(f));
+	f.sucre = 0;
+	ASSERT(not porteSucre(f));	
+}
+
+void testRentreNid(){
+	fourmi f;
+	f.sucre = 3;
+	ASSERT(rentreNid(f));
+	f.sucre = 0;
+	ASSERT(not rentreNid(f));
+}
+
+void testDechargerSucre(){
+	fourmi f;
+	f.sucre = 2;
+	dechargerSucre(f);
+	ASSERT(f.sucre == 0);
+	f.sucre = 0;
+	dechargerSucre(f);
+	ASSERT(f.sucre == 0);
+}
+
+void testChargerSucre(){
+	fourmi f;
+	f.sucre = 2;
+	chargerSucre(f);
+	ASSERT(f.sucre == 3);
+	f.sucre = 0;
+	chargerSucre(f);
+	ASSERT(f.sucre == 1);
+}
+
+void testDeplaceFourmi(){
+	fourmi f;
+	coord c;
+	c.x = 2; c.y = 3;
+	deplaceFourmi(f,c);
+	ASSERT(f.c == c);
+	c.x = 0; c.y = 0;
+	deplacefourmi(f,c);
+	ASSERT(f.c == c);
+}
+
+
 int main(){
 	tabFourmi tf;
 	ensCoord ec_f = nouvEnsCoord();
